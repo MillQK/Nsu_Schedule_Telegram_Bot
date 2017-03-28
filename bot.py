75db3aa97ce12c455945283be4bde4bb370b80a9
Nikita Nikolenko <Nikita@MacBook-Pro-Nikita.local>
2017-03-28 23:27:28 +0700

    Add new commands
    New commands
    Key-value storage

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+from telebot import types
 from random import randint
 import json
+import pickledb
+
 
 bot = telebot.TeleBot(config.token)
+groups_storage = pickledb.load('groups_storage.db', False)
+
 
 @bot.message_handler(commands=['fcoin'])
 def flip_coin(message):
@@ -47,12 +52,20 @@ subjs_dict = {'первая': '0',
               '4': '3',
               '5': '4',
               '6': '5',
-              '7': '6'}
+              '7': '6',
+              'первая (9:00)': '0',
+              'вторая (10:50)': '1',
+              'третья (12:40)': '2',
+              'четвертая (14:30)': '3',
+              'пятая (16:20)': '4',
+              'шестая (18:10)': '5',
+              'седьмая (20:00)': '6'}
 
 EMPTY_SUBJ = 'empty_pair'
+GROUP_NAME = 'group_name'
 
 
-@bot.message_handler(commands=['sch'])
+@bot.message_handler(func=lambda msg: len(msg.text.split()) > 1, commands=['sch'])
 def get_schedule(message):
     information = message.text.split()
 
@@ -75,10 +88,72 @@ def get_schedule(message):
     bot.send_message(message.chat.id, answer)
 
 
+chat_history = dict()
+
+
+@bot.message_handler(commands=['sch'])
+def schedule_custom_keyboard(message):
+    sent = bot.send_message(message.chat.id, 'Введите вашу группу.')
+    bot.register_next_step_handler(sent, dialog_group_check)
+
+
+def dialog_group_check(message):
+    group_num = message.text
+    if group_num not in sch:
+        bot.send_message(message.chat.id, 'Нет такой группы. Попробуйте еще раз.')
+        return
+    dialog_group(message.chat.id, group_num)
+
+
+def dialog_group(mchat_id, group):
+    chat_history[mchat_id] = sch[group]
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Понедельник', 'Вторник')
+    markup.row('Среда', 'Четверг')
+    markup.row('Пятница', 'Суббота')
+    # markup.row('Все дни')
+    sent = bot.send_message(mchat_id, 'Выберете день недели', reply_markup=markup)
+    bot.register_next_step_handler(sent, dialog_weekday)
+
+def dialog_weekday(message):
+    weekday = message.text.lower()
+    if weekday not in days_dict:
+        bot.send_message(message.chat.id, 'Нет такого дня недели. Попробуйте еще раз.',
+                         reply_markup=types.ReplyKeyboardHide())
+        return
+
+    chat_history[message.chat.id] = chat_history[message.chat.id][days_dict[weekday]]
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Первая (9:00)', 'Вторая (10:50)')
+    markup.row('Третья (12:40)', 'Четвертая (14:30)')
+    markup.row('Пятая (16:20)', 'Шестая (18:10)')
+    markup.row('Седьмая (20:00)', 'Все пары')
+    sent = bot.send_message(message.chat.id, 'Выберете пару', reply_markup=markup)
+    bot.register_next_step_handler(sent, dialog_answer)
+
+
+def dialog_answer(message):
+    subj = message.text.lower()
+    if subj not in subjs_dict and subj != 'все пары':
+        bot.send_message(message.chat.id, 'Нет такой пары. Попробуйте еще раз.', reply_markup=types.ReplyKeyboardHide())
+        return
+
+    day = chat_history[message.chat.id]
+    if subj == 'все пары':
+        answer = ''
+        for i in range(0, 7):
+            answer += make_subject_message(day[str(i)]) + '\n\n'
+    else:
+        answer = make_subject_message(day[subjs_dict[subj]])
+
+    bot.send_message(message.chat.id, answer, reply_markup=types.ReplyKeyboardHide())
+
+
 def send_sch_error_message(mid):
     error_massage = 'Использование: /sch <номер группы> <день недели (например: пн или понедельник или 1)' \
                     '(опционально)> <номер пары (цифрой, только если есть день недели)(опционально)>' \
-                    '\nНапример: /sch 16202 пт 3'
+                    '\nНапример: /sch 16202 пт 3\n\n' \
+                    'Либо можете просто написать /sch и дальше взаимодействовать с ботом.'
     bot.send_message(mid, error_massage)
 
 
@@ -114,12 +189,70 @@ def make_subject_message(subj):
                 'Пустая пара' if subj[2] == EMPTY_SUBJ else subj[2])
 
 
+@bot.message_handler(func=lambda msg: len(msg.text.split()) > 1, commands=['setgroup'])
+def set_group_in_msg(message):
+    text = message.text.split()
+    if len(text) > 2:
+        setgroup_help_message(message.chat.id)
+        return
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    if text[1] not in sch:
+        bot.send_message(message.chat.id, 'Нет такой группы.')
+        return
 
-@bot.message_handler(content_types=["start"])
+    if save_data_in_storage(str(message.chat.id), GROUP_NAME, text[1]):
+        bot.send_message(message.chat.id, 'Группа сохранена.')
+    else:
+        bot.send_message(message.chat.id, 'Возникла ошибка при сохранении. Попробуйте еще раз.')
+
+
+@bot.message_handler(commands=['setgroup'])
+def set_group_in_msg(message):
+    text = message.text.split()
+    if len(text) > 1:
+        setgroup_help_message(message.chat.id)
+        return
+
+    if message.text == '/setgroup':
+        send = bot.send_message(message.chat.id, 'Введите вашу группу.')
+        bot.register_next_step_handler(send, set_group_in_msg)
+    else:
+        group = message.text
+        if group not in sch:
+            bot.send_message(message.chat.id, 'Нет такой группы.')
+            return
+
+        if save_data_in_storage(str(message.chat.id), GROUP_NAME, group):
+            bot.send_message(message.chat.id, 'Группа сохранена.')
+        else:
+            bot.send_message(message.chat.id, 'Возникла ошибка при сохранении. Попробуйте еще раз.')
+
+
+def setgroup_help_message(mid):
+    text = 'Использование: /setgroup <номер группы> или /setgroup'
+    bot.send_message(mid, text)
+
+
+def save_data_in_storage(storage_key, data_key, data_value):
+    if storage_key not in groups_storage.getall():
+        return groups_storage.set(storage_key, {data_key: data_value}) and groups_storage.dump()
+    else:
+        data = groups_storage.get(storage_key)
+        data[data_key] = data_value
+        return groups_storage.set(storage_key, data) and groups_storage.dump()
+
+
+@bot.message_handler(commands=["mysch"])
+def dialog_mysch(message):
+    chat_id_str = str(message.chat.id)
+    if chat_id_str not in groups_storage.getall():
+        bot.send_message(message.chat.id, 'У вас неустановленна группа. Воспользуйтесь коммандой /setgroup.')
+        return
+
+    dialog_group(message.chat.id, groups_storage.get(chat_id_str)[GROUP_NAME])
+
+
+@bot.message_handler(commands=["start"])
 def repeat_all_messages(message):
     send_sch_error_message(message.chat.id)
 
