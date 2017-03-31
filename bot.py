1c91038bd9f8933fb3bf29d91b238d0798565b45
Nikita Nikolenko <Nikita@MacBook-Pro-Nikita.local>
2017-03-31 10:37:43 +0700

    Add webhook

--- bot.py
+++ bot.py
@@ -5,10 +5,38 @@ from telebot import types
 from random import randint
 import json
 import pickledb
+import cherrypy
 
 
 bot = telebot.TeleBot(config.token)
 groups_storage = pickledb.load('groups_storage.db', False)
+
+
+WEBHOOK_HOST = 'MilQ.pythonanywhere.com'
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = 'MilQ.pythonanywhere.com'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './nsuSchTelBot_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './nsuSchTelBot_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.token)
+
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # Эта функция обеспечивает проверку входящего сообщения
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
 
 
 def is_command(text):
@@ -274,4 +302,20 @@ def repeat_all_messages(message):
 if __name__ == '__main__':
     with open('sch.txt', 'r') as inp:
         sch = json.load(inp)
-    bot.polling(none_stop=True)
+
+    bot.remove_webhook()
+
+    # Ставим заново вебхук
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                    certificate=open(WEBHOOK_SSL_CERT, 'r'))
+    # bot.polling(none_stop=True)
+    cherrypy.config.update({
+        'server.socket_host': WEBHOOK_LISTEN,
+        'server.socket_port': WEBHOOK_PORT,
+        'server.ssl_module': 'builtin',
+        'server.ssl_certificate': WEBHOOK_SSL_CERT,
+        'server.ssl_private_key': WEBHOOK_SSL_PRIV
+    })
+
+    # Собственно, запуск!
+    cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
